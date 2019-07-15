cd72064ec593b814b55672261b8c415f85866741
rurusyu <Chrizard006@protonmail.com>
2019-07-15 16:48:42 +0900

    Fixed #1 complete

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -4,20 +4,27 @@ import "./Admin.scss";
 class Admin extends Component {
   render() {
     return (
-      <div className="root_header_admin">
-        <div className="header_logo">adminPage</div>
-        <div className="wrap_admin_user_info">
-          <span className="admin_user_info center_name_title">지점명</span>
-          <span className="admin_user_info center_name_name">선릉역</span>
-          <span className="admin_user_info user_position_title">직책명</span>
-          <span className="admin_user_info user_position_name">매니져</span>
-          <span className="admin_user_info admin_user_name_title">
-            관리자명
-          </span>
-          <span className="admin_user_info admin_user_name">아이유</span>
+      <>
+        <header className="root_header_admin">
+          <div className="header_logo">adminPage</div>
+          <div className="wrap_admin_user_info">
+            <span className="admin_user_info">지점명</span>
+            <span className="admin_user_info">선릉역</span>
+            <span className="admin_user_info">직책명</span>
+            <span className="admin_user_info">매니져</span>
+            <span className="admin_user_info">관리자명</span>
+            <span className="admin_user_info">아이유</span>
+          </div>
+          <button className="admin_login_button">로그아웃</button>
+        </header>
+        <div className="root_body_admin">
+          <div className="wrap_order_list">
+            <div className="order_list_title">주문목록</div>
+            <div className="wrap_order_list_card"></div>
+            <div className="wrap_order_list_graph"></div>
+          </div>
         </div>
-        <button className="admin_login_button">로그아웃</button>
-      </div>
+      </>
     );
   }
 }
