dca42a1bd7e72df6341e210a1c2eaab7439fdbb9
rurusyu <Chrizard006@protonmail.com>
2019-07-15 16:13:28 +0900

    admin header 생성완료

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -3,7 +3,22 @@ import "./Admin.scss";
 
 class Admin extends Component {
   render() {
-    return <h1>헬로 가이즈!</h1>;
+    return (
+      <div className="root_header_admin">
+        <div className="header_logo">adminPage</div>
+        <div className="wrap_admin_user_info">
+          <span className="admin_user_info center_name_title">지점명</span>
+          <span className="admin_user_info center_name_name">선릉역</span>
+          <span className="admin_user_info user_position_title">직책명</span>
+          <span className="admin_user_info user_position_name">매니져</span>
+          <span className="admin_user_info admin_user_name_title">
+            관리자명
+          </span>
+          <span className="admin_user_info admin_user_name">아이유</span>
+        </div>
+        <button className="admin_login_button">로그아웃</button>
+      </div>
+    );
   }
 }
 
