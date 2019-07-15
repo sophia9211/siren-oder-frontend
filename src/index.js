d412ac7d12d7d835d9cc4db3ce5c1a43b95c884d
rurusyu <Chrizard006@protonmail.com>
2019-07-15 22:34:00 +0900

    login인증 리덕스 스토어, 리듀서 작성, 리덕스 설치완료

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Routes from "./Routes";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/index";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById("root")
+);
