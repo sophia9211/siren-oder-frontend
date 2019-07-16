2a755d002f3420100cf31bb2f23b64773b1e13c7
sophia <your_email@youremail.com>
2019-07-16 23:51:53 +0900

    디테일 메뉴페이지에서 모달 구성완료 및 컵픽 컨포넌트 생성및 이벤트 적용완료

--- src/Pages/TotalMenu/TotalMenu.js
+++ src/Pages/TotalMenu/TotalMenu.js
@@ -3,61 +3,121 @@ import "./TotalMenu.scss";
 import MainLayout from "Layouts/MainLayout";
 import CategoryButton from "Components/CategoryButton";
 import MenuTicket from "Components/MenuTicket";
+import { Link } from "react-router-dom";
+
+const sampleData = [
+  {
+    categoryName: "음료",
+    categoryId: 1,
+    subCategorys: [
+      {
+        subCategoryName: "콜드브루",
+        subCategoryId: 1
+      },
+      {
+        subCategoryName: "에스프레소",
+        subCategoryId: 2
+      },
+      {
+        subCategoryName: "리저브",
+        subCategoryId: 3
+      },
+      {
+        subCategoryName: "에스프레소",
+        subCategoryId: 4
+      },
+      {
+        subCategoryName: "야이야이",
+        subCategoryId: 5
+      },
+      {
+        subCategoryName: "에스프레소",
+        subCategoryId: 6
+      }
+    ]
+  },
+  {
+    categoryName: "푸드",
+    categoryId: 2,
+    subCategorys: [
+      {
+        subCategoryName: "케익",
+        subCategoryId: 1
+      },
+      {
+        subCategoryName: "쿠키",
+        subCategoryId: 2
+      }
+    ]
+  }
+];
+
+const myData = {};
+sampleData.map((el, idx) => {
+  console.log(el);
+
+  myData[el.categoryId] = el.subCategorys;
+  console.log(myData);
+});
 
 class TotalMenu extends React.Component {
+  state = {
+    id: 0
+  };
+
+  componentDidMount() {
+    this.setState({ id: 1 });
+  }
+
+  handleClick = a => {
+    this.setState({ id: a.toString() });
+    console.log(a);
+  };
+
   render() {
+    console.log(myData);
+    console.log(this.state.id);
+
     return (
       <MainLayout>
         <div className="total_menu_category_box">
-          <CategoryButton
-            className="total_menu_category_btn"
-            hoverClass="total_mene_btn_hover_bar"
-          >
-            음료
-          </CategoryButton>
-          <CategoryButton className="total_menu_category_btn">
-            푸드
-          </CategoryButton>
-          <CategoryButton className="total_menu_category_btn">
-            상품
-          </CategoryButton>
-          <CategoryButton className="total_menu_category_btn">
-            홀케이크 예약
-          </CategoryButton>
+          {sampleData.map((el, idx) => {
+            return (
+              <CategoryButton
+                className="total_menu_category_btn"
+                hoverClass="total_mene_btn_hover_bar"
+                onClick={e => {
+                  this.handleClick(el.categoryId);
+                }}
+              >
+                {el.categoryName}
+              </CategoryButton>
+            );
+          })}
         </div>
         <div className="total_menu_category_box">
-          <CategoryButton
-            className="menu_category_btn"
-            hoverBox="mene_btn_hover_box"
-          >
-            NEW
-          </CategoryButton>
-          <CategoryButton
-            className="menu_category_btn"
-            hoverBox="mene_btn_hover_box"
-          >
-            추천
-          </CategoryButton>
-          <CategoryButton
-            className="menu_category_btn"
-            hoverBox="mene_btn_hover_box"
-          >
-            콜드 브루
-          </CategoryButton>
-          <CategoryButton
-            className="menu_category_btn"
-            hoverBox="mene_btn_hover_box"
-          >
-            리저브
-          </CategoryButton>
+          {myData[this.state.id] &&
+            Array.isArray(myData[this.state.id]) &&
+            myData[this.state.id].map((el, idx) => {
+              return (
+                <CategoryButton
+                  className="menu_category_btn"
+                  hoverBox="mene_btn_hover_box"
+                >
+                  {el.subCategoryName}
+                </CategoryButton>
+              );
+            })}
         </div>
         <div>
-          <MenuTicket
-            menuName="돌체 콜드 브루"
-            engName="Dolce Cold Brew"
-            price="4,100원"
-            imgSrc="http://image.istarbucks.co.kr/upload/store/skuimg/2019/04/[9200000002081]_20190409153909859.jpg"
-          />
+          <Link to="./DetailMenu">
+            <MenuTicket
+              menuName="돌체 콜드 브루"
+              engName="Dolce Cold Brew"
+              price="4,100원"
+              imgSrc="http://image.istarbucks.co.kr/upload/store/skuimg/2019/04/[9200000002081]_20190409153909859.jpg"
+            />
+          </Link>
           <MenuTicket
             menuName="제주 말차샷 크림 프라푸치노"
             engName="Jeju Matcha Shot Cream Frappuccino"
--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Main from "./Pages/Main";
 import TotalMenu from "./Pages/TotalMenu";
 import AdminLogin from "./Pages/Admin/AdminLogin";
 import AdminSignup from "./Pages/Admin/AdminSignup";
+import DetailMenu from "./Pages/DetailMenu";
 
 class Routes extends Component {
   render() {
@@ -13,9 +14,10 @@ class Routes extends Component {
         <Switch>
           <Route exact path="/admin" component={Admin} />
           <Route exact path="/" component={Main} />
-          <Route exact path="/TotalMenu" component={TotalMenu} />
+          <Route exact path="/totalmenu" component={TotalMenu} />
           <Route exact path="/admin/login" component={AdminLogin} />
           <Route exact path="/admin/signup" component={AdminSignup} />
+          <Route exact path="/detailmenu" component={DetailMenu} />
         </Switch>
       </Router>
     );
