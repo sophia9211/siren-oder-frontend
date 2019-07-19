48816209e99be1c958849ab8b4ecb8602bd58df9
ahndohyun <ahndohyun@gmail.com>
2019-07-19 12:43:43 +0900

    로그인 api fetch부분 utils에 정의

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -7,6 +7,6 @@ const ICON =
 const CLOSE_BTN =
   "https://cdn0.iconfinder.com/data/icons/solid-line-essential-ui-icon-set/512/essential_set_close-256.png";
 
-const API_URL = "http://10.58.5.97:8000";
+const API_URL = "http://10.58.0.219:8000";
 
 export { API_URL, LOGO, CLOSE_BTN, ICON };
