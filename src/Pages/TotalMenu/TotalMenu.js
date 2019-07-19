64b8905d72ad85ebbde91c02033097b22df0860a
sophia <your_email@youremail.com>
2019-07-19 19:31:57 +0900

    컨플릿 재수정

--- src/Pages/TotalMenu/TotalMenu.js
+++ src/Pages/TotalMenu/TotalMenu.js
@@ -3,7 +3,6 @@ import "./TotalMenu.scss";
 import MainLayout from "Layouts/MainLayout";
 import CategoryButton from "Components/CategoryButton";
 import MenuTicket from "Components/MenuTicket";
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
 
 const sampleData = [
@@ -60,8 +59,6 @@ sampleData.map((el, idx) => {
   myData[el.categoryId] = el.subCategorys;
   console.log(myData);
 });
-=======
->>>>>>> e25dd27fa04a02afbc35b53c0c9a59dc36830ef0
 
 class TotalMenu extends React.Component {
   state = {
