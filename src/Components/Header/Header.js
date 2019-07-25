70da1d498ba11257c0ba34b1752468a960efa419
ahndohyun <ahndohyun@gmail.com>
2019-07-26 01:12:40 +0900

    fixed 헤더 필요없는 부분 삭제 - #40

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,11 +7,12 @@ class Header extends React.Component {
     return (
       <header>
         <div className="header_box">
-          <div className="header_angel_logo"></div>
+          <Link to="./">
+            <div className="header_angel_logo"></div>
+          </Link>
           <Link to="./">
             <div className="header_text_logo"></div>
           </Link>
-          <div className="header_search_icon"></div>
           <div className="header_shopping_icon"></div>
         </div>
       </header>
